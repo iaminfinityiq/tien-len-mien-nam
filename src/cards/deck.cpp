3ecd3835a42152c9581eb8688f1e1190ed28e5c8
iaminfinityiq <nguyenbaoan.hn2408@gmail.com>
2026-03-21 14:34:00 +0700

    the deck is shuffled now :)

--- src/cards/deck.cpp
+++ src/cards/deck.cpp
@@ -1,4 +1,5 @@
 #include "deck.hpp"
+#include <random>
 
 namespace cards {
     Deck::Deck() {
@@ -24,5 +25,23 @@ namespace cards {
             CardValue::Ace,
             CardValue::Two
         };
+
+        std::vector<Card> dummy = {};
+        for (const CardType &type : this->types) {
+            for (const CardValue &value : this->values) {
+                dummy.push_back(Card(type, value));
+            }
+        }
+
+        std::random_device rd;
+        std::mt19937 gen(rd());
+
+        while (!dummy.empty()) {
+            std::uniform_int_distribution<size_t> roll(0, dummy.size()-1);
+            size_t position = roll(gen);
+
+            this->cards.push_back(dummy[position]);
+            dummy.erase(dummy.begin() + position);
+        }
     }
 }
